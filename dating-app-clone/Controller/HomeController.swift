1350a38c4083d34406e27f8b62a4e6756fcf3c8f
masterambi <ramzyrash@gmail.com>
2020-06-29 23:41:02 +0700

    Add project files

---
+++ dating-app-clone/Controller/HomeController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeController.swift
+//  dating-app-clone
+//
+//  Created by Ramzy Rashaun Arief on 29/06/20.
+//  Copyright © 2020 Ramzy Rashaun Arief. All rights reserved.
+//
+
+import UIKit
+
+class HomeController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+    
+}
